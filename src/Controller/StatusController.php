00c9dc0b47b16deb59293a3e526084bda50f2c11
cchampou <clement@champouillon.com>
2021-03-21 11:11:33 +0100

    feat: add a status route

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatusController extends AbstractController {
+
+    /**
+     * @Route("/status", methods={"GET"})
+     * @return Response
+     */
+    public function status(): Response
+    {
+        return new Response("OK");
+    }
+}
