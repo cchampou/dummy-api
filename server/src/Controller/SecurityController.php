06aa42259dfba09f7476c40e32b7a0ba1c1c0faf
cchampou <clement@champouillon.com>
2021-03-29 00:23:10 +0200

    feat: signup route and user entity created

---
+++ server/src/Controller/SecurityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',
+        ]);
+    }
+
+    /**
+    * @Route("/signup", name="app_signup", methods={"POST"})
+    */
+    public function signup(Request $request, ValidatorInterface $validator, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $user = new User();
+      $user->setEmail($request->request->get('email'));
+      $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+      $errors = $validator->validate($user);
+
+      if(count($errors) > 0) {
+          return new Response("BAD REQUEST", 400);
+      }
+
+
+      $entityManager->persist($user);
+      $entityManager->flush();
+
+      return $this->json([
+        'message' => 'success'
+      ]);
+    }
+}
